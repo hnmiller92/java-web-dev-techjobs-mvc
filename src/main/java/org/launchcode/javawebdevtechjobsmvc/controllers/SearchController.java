4ad2bf722276450968a01c99cd409e6913adc4ce
Hannah Miller <hnmiller92@gmail.com>
2020-06-08 19:54:39 -0500

    Through Step 2

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,9 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+//    @PostMapping
+//    public String displaySearchResults(Model model) {
+//
+//    }
 
 }
